1dcb94aa2b4207d4b8c45af5039c68b866e75728
AlexeySmirnov2 <alexeyus1@gmail.com>
2017-12-16 16:07:38 +0200

    Darbas su index.php: botas atsako.

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
+$questions = array(
+    array("Kiek tau metu?", "40", "20"),
+    array("Koks tavo svoris?", "20", "100")
+);
+$questionsC = count($questions);
+
 include (__DIR__ . '/vendor/autoload.php');
 
-$access_token = 'your_access_token_here';
+// for connection
+$access_token = 'EAAB9YNjCHZAwBAJQp02wRI75C1bJdRwLAQETebAOylqvDeyEkelTzq02lEWbD3eFutAuw0IM9C57kj5VQK0vgnZCydxRapZCOZBHnZBGZC6FZAJzDJu1Y1n7b2NyOqZAbnqyvsxP1WrZBzwaZClbvZBs9oVvCfIEoMufgq7rYsgPqCnBQZDZD';
 $verify_token = 'TOKEN';
-$appId = 'your_app_id_here';
-$appSecret = 'your_app_secret_here';
+$appId = '137854906932636';
+$appSecret = '3ecc648d697ebbd840902a14b8f044d5';
 
 if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
@@ -14,28 +21,58 @@ if(isset($_REQUEST['hub_challenge'])) {
     }
 }
 
-$input = json_decode(file_get_contents('php://input'), true);
+$input = json_decode(file_get_contents('php://input'), true); // to json
 
 if ($input === null) {
     exit;
 }
 
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+// // responce 2
+// $sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ???
+// $data = [
+//     'messaging_type' => 'RESPONSE', // type
+//     'recipient' => [
+//         'id' => $sender,
+//     ],
+//     'message' => [
+//         'text' => 'lalalal',
+//     ]
+// ];
 
+// $response = $fb->post(
+//     '/me/messages', // ????
+//     $data, //
+//     $access_token); // key
+
+// $client = new GuzzleHttp\Client();
+
+    
+// apdorojimas
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ???
+
+// connect for responce
 $fb = new \Facebook\Facebook([
     'app_id' => $appId,
     'app_secret' => $appSecret,
 ]);
 
+
+// responce
 $data = [
-    'messaging_type' => 'RESPONSE',
+    'messaging_type' => 'RESPONSE', // type
     'recipient' => [
         'id' => $sender,
     ],
     'message' => [
-        'text' => 'You wrote: ' . $message,
+//         'text' => 'You wrote: ' . $quations[0][0] . $message,
+        'text' => 'Klausimas: ' . $questions[0][0],
     ]
 ];
 
-$response = $fb->post('/me/messages', $data, $access_token);
+$response = $fb->post(
+    '/me/messages', // ????
+    $data, // 
+    $access_token); // key
+
+    
