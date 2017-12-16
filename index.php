e02b75958cfafde7d1c1c7cbb7ea0efd1966fe6e
AlexeySmirnov2 <alexeyus1@gmail.com>
2017-12-16 22:20:57 +0200

    Pagaliau vel veikia, reikejo node.js ir calbacko.

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <?php
 
+// quations 
 $questions = array(
-    array("Kiek tau metu?", "40", "20"),
-    array("Koks tavo svoris?", "20", "100")
+    array("1", "Kaip vaidimi zmonės, kurie plaukioja laivaias ir plėšia kitus laivus?", 
+        "Degeneratai", "Tomatai", "Seimūnai", 
+        'atsakymas' => "Piratai"),
+    array("4", "Koks chemines lenteles elementas pavadintas piktos dvasios, nykstuko vardu? ",
+        "Hafnis",
+        "Kobaltas",
+        "Berilis", 'atsakymas' => "Kobaltas"),
+    array("5", "Mažiausia valstybė pagal gyventoju skaiciu? ",
+        "Nauru",
+        "Niujė",
+        "Vatikanas", 'atsakymas' => "Vatikanas")
 );
-$questionsC = count($questions);
+$countQ = count($questions);
+//
+
 
 include (__DIR__ . '/vendor/autoload.php');
 
-// for connection
-$access_token = 'EAAB9YNjCHZAwBAJQp02wRI75C1bJdRwLAQETebAOylqvDeyEkelTzq02lEWbD3eFutAuw0IM9C57kj5VQK0vgnZCydxRapZCOZBHnZBGZC6FZAJzDJu1Y1n7b2NyOqZAbnqyvsxP1WrZBzwaZClbvZBs9oVvCfIEoMufgq7rYsgPqCnBQZDZD';
+$access_token = 'EAAB9YNjCHZAwBADOe5Qslk6rBlKvgZBbV3H3eefwZADSRv4mCq4w6jnrXYZCruuhTtkqQWjfc00iq6ArpT4XZBEZA2heuVlBjCyz1T3ZBHxGcQTzX70JspSVLXTXlWzlfxqb4ZBBHdSrxqTf9mdCW1qmFbAbHKBXEGSlSYTugjN1PQZDZD';
 $verify_token = 'TOKEN';
 $appId = '137854906932636';
 $appSecret = '3ecc648d697ebbd840902a14b8f044d5';
+
+
 
 if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
@@ -21,58 +34,28 @@ if(isset($_REQUEST['hub_challenge'])) {
     }
 }
 
-$input = json_decode(file_get_contents('php://input'), true); // to json
+$input = json_decode(file_get_contents('php://input'), true);
 
 if ($input === null) {
     exit;
 }
 
-// // responce 2
-// $sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ???
-// $data = [
-//     'messaging_type' => 'RESPONSE', // type
-//     'recipient' => [
-//         'id' => $sender,
-//     ],
-//     'message' => [
-//         'text' => 'lalalal',
-//     ]
-// ];
 
-// $response = $fb->post(
-//     '/me/messages', // ????
-//     $data, //
-//     $access_token); // key
-
-// $client = new GuzzleHttp\Client();
-
-    
-// apdorojimas
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
-$sender = $input['entry'][0]['messaging'][0]['sender']['id']; // ???
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 
-// connect for responce
 $fb = new \Facebook\Facebook([
     'app_id' => $appId,
     'app_secret' => $appSecret,
 ]);
 
-
-// responce
 $data = [
-    'messaging_type' => 'RESPONSE', // type
+    'messaging_type' => 'RESPONSE',
     'recipient' => [
         'id' => $sender,
     ],
     'message' => [
-//         'text' => 'You wrote: ' . $quations[0][0] . $message,
-        'text' => 'Klausimas: ' . $questions[0][0],
+        'text' => 'You wrote: ' . $message . ' '. $countQ,
     ]
 ];
-
-$response = $fb->post(
-    '/me/messages', // ????
-    $data, // 
-    $access_token); // key
-
-    
+$response = $fb->post('/me/messages', $data, $access_token);
